2c215f7cf14d618e6c02cc9839e08cdaf287d766
lance <lanceren@sjtu.edu.cn>
2021-01-17 16:14:04 +0800

    add bubble train.py and update data path

--- playground/bubble.py
+++ playground/bubble.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
     predict_bubble = {}
     for i in output_space:
         predict_bubble[f'{i}'] = np.array([])
-    predict_0_bubble = np.array([])
     total = 0
     correct = 0
     model_raw, ds_fetcher, is_imagenet = selector.select('mnist')
@@ -22,7 +21,7 @@ if __name__ == "__main__":
         target_clone = target.clone()
         output_part1, output_part2 = model_raw.multipart_forward(data)
 
-        for i in range(batch_size):
+        for i in range(output_part2.data.cpu().numpy().shape[0]):
             assert torch.argmax(output_part2[i].data) in output_space, "output overflow"
             predict_bubble[f'{torch.argmax(output_part2[i].data)}'] = np.concatenate((predict_bubble[f'{torch.argmax(output_part2[i].data)}'], output_part1[i].data.cpu().numpy()), axis=0)
         activated_bubble = (output_part1.data.cpu().numpy() >= threshold).astype(np.bool)
@@ -31,6 +30,7 @@ if __name__ == "__main__":
         correct = correct + pred.eq(target_clone).sum()
         total = total + len(data)
 
-    predict_0_bubble = predict_0_bubble.reshape((-1, 256))
+    for i in output_space:
+        predict_bubble[f'{i}'] = predict_bubble[f'{i}'].reshape((-1, 256))
     acc = correct * 1.0 / total
     print(f"准确率为{acc}")
